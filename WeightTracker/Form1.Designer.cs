fe8e1456354458cd39b228416ea907d6e7467de3
Natha <Natha@NSTALL-LAPTOP>
2023-12-09 21:56:36 -0600

    Add project files.

---
+++ WeightTracker/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WeightTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.graphButton = new System.Windows.Forms.Button();
+            this.exerciseBox = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.exerciseLabel = new System.Windows.Forms.Label();
+            this.repWeightGrid = new System.Windows.Forms.DataGridView();
+            this.RepsCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.WeightCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addSetButton = new System.Windows.Forms.Button();
+            this.addExerciseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.repWeightGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(550, 201);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // graphButton
+            // 
+            this.graphButton.Location = new System.Drawing.Point(550, 230);
+            this.graphButton.Name = "graphButton";
+            this.graphButton.Size = new System.Drawing.Size(75, 23);
+            this.graphButton.TabIndex = 1;
+            this.graphButton.Text = "Graph";
+            this.graphButton.UseVisualStyleBackColor = true;
+            // 
+            // exerciseBox
+            // 
+            this.exerciseBox.FormattingEnabled = true;
+            this.exerciseBox.Items.AddRange(new object[] {
+            "Bench Press",
+            "Squat",
+            "Deadlift",
+            "Lat Pulldown",
+            "Bicep Curls",
+            "Tricep Extensions",
+            "Shoulder Press",
+            "Lat Rows",
+            "Leg Extension",
+            "Hamstring Curls",
+            "Shoulder Shrugs ",
+            "Calf Raises",
+            "Pull-ups ",
+            "Push-ups",
+            "Shoulder Raises"});
+            this.exerciseBox.Location = new System.Drawing.Point(33, 58);
+            this.exerciseBox.Name = "exerciseBox";
+            this.exerciseBox.Size = new System.Drawing.Size(156, 21);
+            this.exerciseBox.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(30, 94);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // exerciseLabel
+            // 
+            this.exerciseLabel.AutoSize = true;
+            this.exerciseLabel.Location = new System.Drawing.Point(30, 42);
+            this.exerciseLabel.Name = "exerciseLabel";
+            this.exerciseLabel.Size = new System.Drawing.Size(53, 13);
+            this.exerciseLabel.TabIndex = 6;
+            this.exerciseLabel.Text = "Exercise: ";
+            // 
+            // repWeightGrid
+            // 
+            this.repWeightGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.repWeightGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RepsCol,
+            this.WeightCol});
+            this.repWeightGrid.Location = new System.Drawing.Point(285, 58);
+            this.repWeightGrid.Name = "repWeightGrid";
+            this.repWeightGrid.Size = new System.Drawing.Size(244, 195);
+            this.repWeightGrid.TabIndex = 10;
+            this.repWeightGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.repWeightGrid_CellContentClick);
+            // 
+            // RepsCol
+            // 
+            this.RepsCol.HeaderText = "Reps";
+            this.RepsCol.Name = "RepsCol";
+            this.RepsCol.ReadOnly = true;
+            // 
+            // WeightCol
+            // 
+            this.WeightCol.HeaderText = "Weight";
+            this.WeightCol.Name = "WeightCol";
+            this.WeightCol.ReadOnly = true;
+            // 
+            // addSetButton
+            // 
+            this.addSetButton.Location = new System.Drawing.Point(196, 58);
+            this.addSetButton.Name = "addSetButton";
+            this.addSetButton.Size = new System.Drawing.Size(75, 23);
+            this.addSetButton.TabIndex = 11;
+            this.addSetButton.Text = "add set";
+            this.addSetButton.UseVisualStyleBackColor = true;
+            this.addSetButton.Click += new System.EventHandler(this.addSetButton_Click);
+            // 
+            // addExerciseButton
+            // 
+            this.addExerciseButton.Location = new System.Drawing.Point(550, 58);
+            this.addExerciseButton.Name = "addExerciseButton";
+            this.addExerciseButton.Size = new System.Drawing.Size(75, 42);
+            this.addExerciseButton.TabIndex = 12;
+            this.addExerciseButton.Text = "add exercise";
+            this.addExerciseButton.UseVisualStyleBackColor = true;
+            this.addExerciseButton.Click += new System.EventHandler(this.addExerciseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 486);
+            this.Controls.Add(this.addExerciseButton);
+            this.Controls.Add(this.addSetButton);
+            this.Controls.Add(this.repWeightGrid);
+            this.Controls.Add(this.exerciseLabel);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.exerciseBox);
+            this.Controls.Add(this.graphButton);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.repWeightGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button graphButton;
+        private System.Windows.Forms.ComboBox exerciseBox;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label exerciseLabel;
+        private System.Windows.Forms.DataGridView repWeightGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RepsCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn WeightCol;
+        private System.Windows.Forms.Button addSetButton;
+        private System.Windows.Forms.Button addExerciseButton;
+    }
+}
+
---
+++ WeightTracker/Form1.cs
@@ -0,0 +1,200 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WeightTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            repWeightGrid.CellValidating += repWeightGrid_CellValidating;
+        }
+
+        private void avgWeightLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void repWeightGrid_CellContentClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addSetButton_Click(object sender, EventArgs e)
+        {
+            repWeightGrid.Rows.Add();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            //Store Exercise Object with all data in winform to file of data
+            Workout w = new Workout();
+            
+
+            string content = JsonConvert.SerializeObject(w);
+            String filePath = "workoutData.json";
+            File.AppendAllText(filePath, content);
+        }
+
+        // When clicked, add all field values into a new exercise object
+        private void addExerciseButton_Click(object sender, EventArgs e)
+        {
+            //creating values for Exercise 
+            String name = exerciseBox.Text;
+            int sets = repWeightGrid.RowCount;
+
+            // From the table in the window, generate lists of reps and weights
+            int[] repList = new int[sets];
+            int[] weightList = new int[sets];
+            for (int i = 0; i < sets; i++)
+            {
+                repList[i] = (int)repWeightGrid.Rows[i].Cells[0].Value;
+                weightList[i] = (int)repWeightGrid.Rows[i].Cells[1].Value;
+            }
+
+            Exercise ex = new Exercise(name, sets, repList, weightList);
+            // add to some workout object 
+
+            // display to user that it worked
+            MessageBox.Show("Exercise added");
+
+        }
+
+        // ensure only numbers are entered into the data grid
+        private void repWeightGrid_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            DataGridView dataGridView = (DataGridView)sender;
+            string inputValue = e.FormattedValue.ToString();
+
+            // Use a suitable validation method, such as TryParse, to check if the input is a number
+            if (!int.TryParse(inputValue, out _))
+            {
+                // Display an error message
+                MessageBox.Show("Please enter a valid number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Cancel the editing process to keep the focus on the current cell
+                e.Cancel = true;
+            }
+        }
+    }
+
+    // Workout Class
+    public class Workout
+    {
+        
+        private List<Exercise> exercises;   // The list of exercises from the workout
+        private DateTime dateOfWorkout;     // the date the workout was completed
+
+        // return list of exercises
+        public List<Exercise> getExercises()
+        {
+            return this.exercises;
+        }
+
+        // set the entire list of exercises in Workout 
+        public void setExercises(List<Exercise> ex)
+        {
+            this.exercises = ex;
+        }
+
+        // add an exercise to the end of the list
+        // in the future, add a way to sort lists by date
+        public void addExercise(Exercise e)
+        {
+            exercises.Add(e);
+        }
+
+        // removes an exercise from the workout list if it matches the input argument
+        // ideally never removes more than one at a time because of dateOfWorkout
+        public void removeExercise(Exercise e)
+        {
+            foreach (Exercise ex in exercises)
+            {
+                if(ex.Equals(e))
+                {
+                    exercises.Remove(ex);
+                }
+            }
+        }
+
+        // get/set date
+        public DateTime getDateOfWorkout()
+        {
+            return this.dateOfWorkout;
+        }
+
+        public void setDateOfWorkout(DateTime d)
+        {
+            this.dateOfWorkout = d;
+        }
+    }
+
+    // Exercise Class 
+    public class Exercise
+    {
+        private String name;   // name of exercise
+        private int sets;      // number of sets done
+        private int[] reps;    // a list of numbers, one entry per set
+        private int[] weights; // a list of numbers, one entry per set
+
+        public Exercise(string n, int s, int[] r, int[] w)
+        {
+            name = n;
+            sets = s;
+            reps = r;
+            weights = w;
+
+        }
+        // getters and setters
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void setName(String n)
+        {
+            this.name = n;
+        }
+
+        public int getSets()
+        {
+            return this.sets;
+        }
+
+        public void setSets(int s)
+        {
+            this.sets = s;
+        }
+
+        public int[] getReps()
+        {
+            return this.reps;
+        }
+
+        public void setReps(int[] r)
+        {
+            this.reps = r;
+        }
+
+        public int[] getWeights()
+        {
+            return this.weights;
+        }
+
+        public void setWeights(int[] w)
+        {
+            this.weights = w;
+        }
+    }
+}
